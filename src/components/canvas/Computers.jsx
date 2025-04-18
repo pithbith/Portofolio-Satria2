d896ce96f244e4733e340dcc6008efa28ba74976
pithbith <pithbith@gmail.com>
2025-04-18 08:10:46 +0700

    Files upload

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,7 +5,7 @@
   import CanvasLoader from "../Loader";
 
   const Computers = ({ isMobile }) => {
-    const computer = useGLTF("./desktop_pc/scene-v1.glb");
+    const computer = useGLTF("./desktop_pc/scene.gltf");
 
     return (
       <mesh>
